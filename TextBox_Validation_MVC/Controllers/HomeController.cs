72303d4165ce04b943ddce057bddf0273dbced08
Karen Payne <paynekaren@comcast.net>
2019-06-14 13:25:15 -0700

    Added code to show how to pass data from controller to view via a model

--- TextBox_Validation_MVC/Controllers/HomeController.cs
+++ TextBox_Validation_MVC/Controllers/HomeController.cs
@@ -12,12 +12,15 @@ namespace TextBox_Validation_MVC.Controllers
         // GET: Home
         public ActionResult Index()
         {
+            var appInfo = new AppInformation();
+            ViewBag.Message = appInfo;
             return View();
         }
 
         [HttpPost]
         public ActionResult Index(PersonModel person)
         {
+            
             return View(); // Examine with breakpoint
         }
     }
---
+++ TextBox_Validation_MVC/Models/AppInformation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TextBox_Validation_MVC.Models
+{
+    public class AppInformation
+    {
+        public string Today => "Today " + DateTime.Now.ToLongDateString();
+        public string Author => "Author Karen Payne";
+    }
+}
